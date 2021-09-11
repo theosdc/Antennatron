2e8f3216e25da4bbcac8362bea28c370d622cd70
theosdc <t206191@dac.unicamp.br>
2021-09-11 08:24:15 -0300

    Inclusao da licenca

--- antennatron.m
+++ antennatron.m
@@ -1,3 +1,16 @@
+#    Simulador de antenas baseado em Octave   
+#    Copyright (C) 2021  Theo Camargo
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
 clear all; #garante que todas as variaveis estao zeradas
 
 #Variaveis globais
